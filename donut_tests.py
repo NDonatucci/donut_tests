3133152f53e322f32e84d9ed59886028c525c2fd
Nicolas Donatucci <ndonatucci@medallia.com>
2021-02-23 18:50:21 -0300

    modifying stuf

--- donut_tests.py
+++ donut_tests.py
@@ -22,6 +22,8 @@
 
 from __future__ import print_function
 
+import csv
+
 import numpy
 import json
 
@@ -31,79 +33,45 @@ import scipy.stats
 parser = argparse.ArgumentParser(description='Test data for distinguishability form random, using NIST SP800-22Rev1a algorithms.')
 parser.add_argument('alphabet_size', type=int, help='Size of alphabet to consider')
 parser.add_argument('filename', type=str, nargs='?', help='Filename of binary file to test')
-parser.add_argument('--be', action='store_false',help='Treat data as big endian bits within bytes. Defaults to little endian')
 parser.add_argument('-t', '--testname', default=None,help='Select the test to run. Defaults to running all tests. Use --list_tests to see the list')
 parser.add_argument('--list_tests', action='store_true',help='Display the list of tests')
 parser.add_argument('--mode', default="default", type=str)
 parser.add_argument('--test', default="monobit", type=str)
 parser.add_argument('--config', default="config.json", type=str)
+parser.add_argument('--stream_size', default=10000, type=int)
 
 args = parser.parse_args()
 
-bigendian = args.be
 filename = args.filename
 sigma = args.alphabet_size
 test = args.test
 config_path = args.config
+stream_size = args.stream_size
 
 config = []
 with open(config_path) as config_payload:
     config = json.load(config_payload)
 
+# with open(filename, 'r') as source:
+#     reader = csv.reader(source)
+#     for row in reader:
+#         data = row[0]
+#         stuff = data.split(";")
+#         if stuff[0] != '0':
+#             number = stuff[2]
+#             dest.write(number)
+#             dest.write("\n")
 
-testlist = [test +'_test']
+m = __import__("donut_" + test + "_test")
+func = getattr(m, test + "_test")
+p_values = []
+for i in range(0, 10000):
+    print(i)
+    arr = numpy.random.randint(0, sigma, 100000)
+    (success,p,plist) = func(arr, sigma, config[test])
+    p_values.append(p)
 
-
-if args.mode == "histogram":
-    m = __import__("donut_" + test + "_test")
-    func = getattr(m, test + "_test")
-    p_values = []
-    for i in range(0, 10000):
-        print(i)
-        arr = numpy.random.randint(0, sigma, 100000)
-        (success,p,plist) = func(arr, sigma, config[test])
-        p_values.append(p)
-
-    matplotlib.pyplot.hist(p_values, 10)
-    matplotlib.pyplot.show()
-    statistic, kspvalue = scipy.stats.kstest(p_values, 'uniform')
-    print(kspvalue)
-
-else:
-    arr = numpy.random.randint(0, sigma, 10000)
-    results = list()
-
-    for testname in testlist:
-        print("TEST: %s" % testname)
-        m = __import__ ("donut_"+testname)
-        func = getattr(m,testname)
-
-        (success,p,plist) = func(arr, sigma, config[test])
-
-        summary_name = testname
-        if success:
-            print("  PASS")
-            summary_result = "PASS"
-        else:
-            print("  FAIL")
-            summary_result = "FAIL"
-
-        if p != None:
-            print("  P="+str(p))
-            summary_p = str(p)
-
-        if plist != None:
-            for pval in plist:
-                print("P="+str(pval))
-            summary_p = str(min(plist))
-
-        results.append((summary_name,summary_p, summary_result))
-
-    print()
-    print("SUMMARY")
-    print("-------")
-
-    for result in results:
-        (summary_name,summary_p, summary_result) = result
-        print(summary_name.ljust(40),summary_p.ljust(18),summary_result)
-
+matplotlib.pyplot.hist(p_values, 10)
+matplotlib.pyplot.show()
+statistic, kspvalue = scipy.stats.kstest(p_values, 'uniform')
+print(kspvalue)
